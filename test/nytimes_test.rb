23bc5c9fb032054671327044c76656a72c8b16b8
Emilio <thecraftedgem@tuta.io>
2018-11-12 14:42:08 -0700

    Dragons Conquered

--- test/nytimes_test.rb
+++ test/nytimes_test.rb
@@ -7,17 +7,19 @@ class NytimesTest < Minitest::Test
   attr_reader :hash
 
   def setup
-    @hash = Nytimes::DATA
+    @hash = Nytimes::DATA  
   end
 
   def test_it_can_get_copyright
     #Set your code to the local variable, "result"
+    result = @hash[:copyright]
 
     assert result, "Copyright (c) 2018 The New York Times Company. All Rights Reserved."
   end
 
   def test_it_can_get_array_of_stories
     #Set your code to the local variable, "result"
+    result = @hash[:results]
 
     assert result.is_a? (Array)
     assert_equal 44, result.count
@@ -25,6 +27,10 @@ class NytimesTest < Minitest::Test
 
   def test_it_can_get_all_stories_with_subsection_of_politics
     #Set your code to the local variable, "result"
+    result = @hash[:results].find_all do |element|
+        element[:subsection] == "Politics"
+  end
+ 
 
 
     assert result.is_a? (Array)
